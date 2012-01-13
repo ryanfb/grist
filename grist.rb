edaa7205ffc024ce86b00f581effa8141c658431
Ryan Baumann <ryan.baumann@gmail.com>
2012-01-13 12:00:55 -0500

    List all gists

--- grist.rb
+++ grist.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'sinatra_auth_github'
 require 'yaml'
+require 'haml'
+require 'pp'
+require 'rest_client'
 
-enable :sessions
+enable :sessions, :logging
 
 config = YAML.load_file('config.yml')
 
@@ -13,27 +16,35 @@ set :github_options, {
 
 Sinatra.register Sinatra::Auth::Github
 
+def fetch_all_gists
+  page = 1
+  response = RestClient.get("https://api.github.com/gists", :params => { :access_token => github_user.token }, :accept => :json)
+  gists = JSON.parse(response)
+  while response.headers[:link] =~ /rel..next/ do
+    page += 1
+    puts "Fetching page #{page}"
+    response = RestClient.get("https://api.github.com/gists", :params => { :access_token => github_user.token, :page => page }, :accept => :json)
+    gists += JSON.parse(response)
+  end
+  return gists
+end
+
+
 helpers do
   def repos
     github_request("user/repos")
+  end
+  def gists
+    # session[:gists] ||= 
+    fetch_all_gists
   end
 end
 
 get '/' do
   authenticate!
-  "Hello There, #{github_user.name}!#{github_user.token}\n#{repos.inspect}"
+  puts "test"
+  haml :index
 end
-
-get '/orgs/:id' do
-  github_public_organization_authenticate!(params['id'])
-  "Hello There, #{github_user.name}! You have access to the #{params['id']} organization."
-end
-
-# the scopes above need to include repo for team access :(
-# get '/teams/:id' do
-#   github_team_authenticate!(params['id'])
-#   "Hello There, #{github_user.name}! You have access to the #{params['id']} team."
-# end
 
 get '/logout' do
   logout!
